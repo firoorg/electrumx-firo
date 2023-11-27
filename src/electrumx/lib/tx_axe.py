0ddce29d76f4c4fa8d461ebce70655816d12cc15
levonpetrosyan93 <45027856+levonpetrosyan93@users.noreply.github.com>
2023-11-28 00:45:52 +0400

    Spark (#5)
    * Spark support added
    
    * getsparklatestcoinid  fixed

--- src/electrumx/lib/tx_axe.py
+++ src/electrumx/lib/tx_axe.py
@@ -507,8 +507,23 @@ class FiroLelantusTx(namedtuple("FiroLelantusTx", "lelantusData")):
         deser.cursor += extra_payload_lengh
         return tx
 
+class FiroSparkSpend(namedtuple("FiroSparkSpend", "sparkData")):
+    def serialize(self):
+        res = (
+            self.sparkData
+        )
+        return res
+
+    @classmethod
+    def read_tx_extra(cls, deser, extra_payload_lengh):
+        tx = FiroSparkSpend(deser.binary[deser.cursor:deser.cursor + extra_payload_lengh])
+        deser.cursor += extra_payload_lengh
+        return tx
+
 class DeserializerFiro(DeserializerDash):
     LELANTUS_TX = 8
+    SPARK_SPEND = 9
+
     SPEC_TX_HANDLERS = {
         DeserializerDash.PRO_REG_TX: AxeProRegTx,
         DeserializerDash.PRO_UP_SERV_TX: AxeProUpServTx,
@@ -516,7 +531,8 @@ class DeserializerFiro(DeserializerDash):
         DeserializerDash.PRO_UP_REV_TX: AxeProUpRevTx,
         DeserializerDash.CB_TX: AxeCbTx,
 
-        LELANTUS_TX: FiroLelantusTx
+        LELANTUS_TX: FiroLelantusTx,
+        SPARK_SPEND: FiroSparkSpend
     }
 
     def read_tx(self):
